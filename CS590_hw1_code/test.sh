8150c359d0675f450a268b32de0c202ea73bbe83
sanjeet-jain <sanjeetjain241210@gmail.com>
2022-09-27 23:58:05 -0400

    name correction

--- CS590_hw1_code/test.sh
+++ CS590_hw1_code/test.sh
@@ -5,20 +5,20 @@ echo
 echo "test 10000, 10 Default"
 ./hw1 10000 10 0 0 >> default_insertion_sort_10000_10.txt
 echo "Done"
-echo "test 10000, 10 Default"
+echo "test 10000, 25 Default"
 ./hw1 10000 25 0 0 >> default_insertion_sort_10000_25.txt
 echo "Done"
-echo "test 10000, 10 Default"
+echo "test 10000, 50 Default"
 ./hw1 10000 50 0 0 >> default_insertion_sort_10000_50.txt
 echo "Done"
 
 echo "test 25000, 10 Default"
 ./hw1 25000 10 0 0 >> default_insertion_sort_25000_10.txt
 echo "Done"
-echo "test 25000, 10 Default"
+echo "test 25000, 25 Default"
 ./hw1 25000 25 0 0 >> default_insertion_sort_25000_25.txt
 echo "Done"
-echo "test 25000, 10 Default"
+echo "test 25000, 50 Default"
 ./hw1 25000 50 0 0 >> default_insertion_sort_25000_50.txt
 echo "Done"
 
@@ -26,20 +26,20 @@ echo "Done"
 echo "test 50000, 10 Default"
 ./hw1 50000 10 0 0 >> default_insertion_sort_50000_10.txt
 echo "Done"
-echo "test 50000, 10 Default"
+echo "test 50000, 25 Default"
 ./hw1 50000 25 0 0 >> default_insertion_sort_50000_25.txt
 echo "Done"
-echo "test 50000, 10 Default"
+echo "test 50000, 50 Default"
 ./hw1 50000 50 0 0 >> default_insertion_sort_50000_50.txt
 echo "Done"
 
 echo "test 100000, 10 Default"
 ./hw1 100000 10 0 0 >> default_insertion_sort_100000_10.txt
 echo "Done"
-echo "test 100000, 10 Default"
+echo "test 100000, 25 Default"
 ./hw1 100000 25 0 0 >> default_insertion_sort_100000_25.txt
 echo "Done"
-echo "test 100000, 10 Default"
+echo "test 100000, 50 Default"
 ./hw1 100000 50 0 0 >> default_insertion_sort_100000_50.txt
 echo "Done"
 
@@ -47,10 +47,10 @@ echo "Done"
 echo "test 250000, 10 Default"
 ./hw1 250000 10 0 0 >> default_insertion_sort_250000_10.txt
 echo "Done"
-echo "test 250000, 10 Default"
+echo "test 250000, 25 Default"
 ./hw1 250000 25 0 0 >> default_insertion_sort_250000_25.txt
 echo "Done"
-echo "test 250000, 10 Default"
+echo "test 250000, 50 Default"
 ./hw1 250000 50 0 0 >> default_insertion_sort_250000_50.txt
 echo "Done"
 
@@ -58,10 +58,10 @@ echo "Done"
 echo "test 500000, 10 Default"
 ./hw1 500000 10 0 0 >> default_insertion_sort_500000_10.txt
 echo "Done"
-echo "test 500000, 10 Default"
+echo "test 500000, 25 Default"
 ./hw1 500000 25 0 0 >> default_insertion_sort_500000_25.txt
 echo "Done"
-echo "test 500000, 10 Default"
+echo "test 500000, 50 Default"
 ./hw1 500000 50 0 0 >> default_insertion_sort_500000_50.txt
 echo "Done"
 
@@ -69,20 +69,20 @@ echo "Done"
 echo "test 1000000, 10 Default"
 ./hw1 1000000 10 0 0 >> default_insertion_sort_1000000_10.txt
 echo "Done"
-echo "test 1000000, 10 Default"
+echo "test 1000000, 25 Default"
 ./hw1 1000000 25 0 0 >> default_insertion_sort_1000000_25.txt
 echo "Done"
-echo "test 1000000, 10 Default"
+echo "test 1000000, 50 Default"
 ./hw1 1000000 50 0 0 >> default_insertion_sort_1000000_50.txt
 echo "Done"
 
 echo "test 1250000, 10 Default"
 ./hw1 1250000 10 0 0 >> default_insertion_sort_1250000_10.txt
 echo "Done"
-echo "test 1250000, 10 Default"
+echo "test 1250000, 25 Default"
 ./hw1 1250000 25 0 0 >> default_insertion_sort_1250000_25.txt
 echo "Done"
-echo "test 1250000, 10 Default"
+echo "test 1250000, 50 Default"
 ./hw1 1250000 50 0 0 >> default_insertion_sort_1250000_50.txt
 echo "Done"
 
@@ -90,9 +90,9 @@ echo "Done"
 echo "test 2500000, 10 Default"
 ./hw1 2500000 10 0 0 >> default_insertion_sort_2500000_10.txt
 echo "Done"
-echo "test 2500000, 10 Default"
+echo "test 2500000, 25 Default"
 ./hw1 2500000 25 0 0 >> default_insertion_sort_2500000_25.txt
 echo "Done"
-echo "test 2500000, 10 Default"
+echo "test 2500000, 50 Default"
 ./hw1 2500000 50 0 0 >> default_insertion_sort_2500000_50.txt
 echo "Done"
