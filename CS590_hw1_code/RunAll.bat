b6030653855aaa371095288f4794c7e6dd1f863c
sanjeet-jain <sanjeetjain241210@gmail.com>
2022-09-28 01:33:33 -0400

    Update RunAll.bat

--- CS590_hw1_code/RunAll.bat
+++ CS590_hw1_code/RunAll.bat
@@ -1,21 +1,21 @@
 
 start bash default-random-test.sh
-timeout /t 10 /nobreak
+timeout /t 5 /nobreak
 
 start bash optimised-inverted-test.sh
-timeout /t 10 /nobreak
+timeout /t 5 /nobreak
  
 start bash optimised-sorted-test.sh 
-timeout /t 10 /nobreak
+timeout /t 5 /nobreak
  
 start bash default-inverted-test.sh 
-timeout /t 10 /nobreak
+timeout /t 5 /nobreak
  
 start bash default-sorted-test.sh 
-timeout /t 10 /nobreak
+timeout /t 5 /nobreak
  
 start bash optimised-random-test.sh 
-timeout /t 10 /nobreak
+timeout /t 5 /nobreak
 
 pause
  
