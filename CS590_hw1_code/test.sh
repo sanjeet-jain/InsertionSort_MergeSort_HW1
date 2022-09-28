9977492195b9f076bac968ab23fb74fe5cdc390a
sanjeet-jain <sanjeetjain241210@gmail.com>
2022-09-27 23:47:40 -0400

    sample script

---
+++ CS590_hw1_code/test.sh
@@ -0,0 +1,9 @@
+echo "Starting Testing Script"
+echo
+echo "test 10000, 10 Default"
+./hw1 10000 10 0 0 >> default_insertion_sort_10000_10.txt
+# echo "test 10000, 10 Default"
+# ./hw1 10000 25 0 0 >> default_insertion_sort_10000_25.txt
+# echo "test 10000, 10 Default"
+# ./hw1 10000 50 0 0 >> default_insertion_sort_10000_50.txt
+
